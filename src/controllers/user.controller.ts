a101a5d5304594932c6ae4a9c36fa7c4e590a000
devdrqgon <70473449+devdrqgon@users.noreply.github.com>
2021-11-04 12:08:03 +0100

    login Function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import logging from "../config/logging";
 import bcryptjs from 'bcryptjs'
 import User from '../models/user.model'
+import signJWT from "../functions/signJWT";
 const NAMESPACE = "User"
 
 const validateToken = (req: Request, res: Response, next: NextFunction) => {
@@ -44,7 +45,60 @@ const registerUser = (req: Request, res: Response, next: NextFunction) => {
 }
 
 
-const logInUser = (req: Request, res: Response, next: NextFunction) => { }
+const logInUser = (req: Request, res: Response, next: NextFunction) => {
+    const { username, password } = req.body
+    User.find({ username })
+        .exec()
+        .then(users => {
+            //Check that username is found and unique,  TODO: do check that user is unqiue  in register methode!
+            if (users.length !== 1) {
+                return res.status(401).json({
+                    message: 'Unauthorized'
+                })
+            }
+            const fetchedUser = users[0]
+            //alles gut, let s verify password
+            bcryptjs.compare(
+                password,
+                fetchedUser.password,
+                (error, result) => {
+                    if (error) {
+                        logging.error(NAMESPACE, `Wrong password::  ${error.message}`, error)
+                        //PASSWORD MISSMATCH!
+                        return res.status(401).json({
+                            message: 'Unauthorized'
+                        })
+                    }
+                    else if (result) {
+                        signJWT(fetchedUser, (_error, token) => {
+                            if (_error) {
+                                logging.error(NAMESPACE, 'Unable to sign token', _error)
+
+                                return res.status(401).json({
+                                    message: 'Unauthorized'
+                                })
+                            }
+                            else if (token) {
+                                return res.status(200).json({
+                                    message: 'Auth Successfull',
+                                    token,
+                                    fetchedUser
+                                })
+                            }
+                        })
+                    }
+                }
+            )
+
+        })
+        .catch(_error => {
+            logging.error(NAMESPACE, _error.message, _error)
+            return res.status(500).json({
+                message: _error.message,
+                error: _error
+            })
+        })
+}
 
 
 const getAllUsers = (req: Request, res: Response, next: NextFunction) => { }
